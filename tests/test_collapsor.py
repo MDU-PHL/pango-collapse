5d380726ee37ef242320fdec4d344e85840e0c6c
wytamma <wytamma.wirth@me.com>
2022-09-06 14:04:37 +1000

    :white_check_mark: rename tests

--- tests/test_collapsor.py
+++ tests/test_collapsor.py
@@ -1,7 +1,7 @@
 from pango_collapse import Collapsor
 
 
-def test_find_parent():
+def test_collapse():
     collapsor = Collapsor(alias_file="tests/data/alias_key.json")
     uncompressed_lineage = "B.1.1.529.5"
     parent = collapsor.collapse(uncompressed_lineage, ("BA.5", "B"))
