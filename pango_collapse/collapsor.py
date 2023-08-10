5a41bf70f2fff337ca4f2b5ff4b8d5f0f1d31f61
Wytamma Wirth <wytamma.wirth@me.com>
2023-08-10 11:32:17 +1000

    :sparkles: add expand methods to Collapsor

--- pango_collapse/collapsor.py
+++ pango_collapse/collapsor.py
@@ -2,6 +2,7 @@ from typing import List
 from pango_aliasor.aliasor import Aliasor
 import pandas as pd
 
+
 class Collapsor(Aliasor):
     def __init__(self, alias_file=None):
         super().__init__(alias_file=alias_file)
@@ -9,10 +10,9 @@ class Collapsor(Aliasor):
     def collapse(
         self, compressed_lineage: str, potential_parents: List[str], strict=False
     ):
-            
         if compressed_lineage in potential_parents:
             return compressed_lineage
-        
+
         uncompressed_lineage = self.uncompress(compressed_lineage)
 
         parts = uncompressed_lineage.split(".")
@@ -21,7 +21,7 @@ class Collapsor(Aliasor):
             compressed_parent_lineage = self.compress(".".join(parts[:-i]))
             if compressed_parent_lineage in potential_parents:
                 return compressed_parent_lineage
-        
+
         if uncompressed_lineage.startswith("X") and "Recombinant" in potential_parents:
             # special case for Recombinant
             return "Recombinant"
@@ -31,10 +31,38 @@ class Collapsor(Aliasor):
 
         return compressed_lineage
 
-    def collapse_column(self, array_of_uncompress_lineages, potential_parents, strict=False):
-        return [ self.collapse(
-            compressed_lineage, potential_parents, strict=strict
-        ) if pd.notna(compressed_lineage) else None for compressed_lineage in array_of_uncompress_lineages]
+    def collapse_column(
+        self, array_of_uncompress_lineages, potential_parents, strict=False
+    ):
+        return [
+            self.collapse(compressed_lineage, potential_parents, strict=strict)
+            if pd.notna(compressed_lineage)
+            else None
+            for compressed_lineage in array_of_uncompress_lineages
+        ]
 
     def uncompress_column(self, array_of_compressed_lineages):
-        return [self.uncompress(lineage) if pd.notna(lineage) else None for lineage in array_of_compressed_lineages]
+        return [
+            self.uncompress(lineage) if pd.notna(lineage) else None
+            for lineage in array_of_compressed_lineages
+        ]
+
+    def expand(self, lineage, delimiter=":"):
+        uncompressed_lineage = self.uncompress(lineage)
+
+        parts = uncompressed_lineage.split(".")
+        levels = len(parts) - 1  # (remove starting B/A)
+        indirections = (levels) % 3 if (levels) % 3 != 0 else 3
+
+        expanded_lineage = [self.compress(uncompressed_lineage)]
+        for i in range(indirections, levels, 3):
+            compressed_parent_lineage = self.compress(".".join(parts[:-i]))
+            expanded_lineage.append(compressed_parent_lineage)
+
+        return delimiter.join(expanded_lineage[::-1])
+
+    def expand_column(self, array_of_lineages, delimiter=":"):
+        return [
+            self.expand(lineage, delimiter=delimiter) if pd.notna(lineage) else None
+            for lineage in array_of_lineages
+        ]
--- tests/test_collapsor.py
+++ tests/test_collapsor.py
@@ -8,17 +8,38 @@ def test_collapse():
     assert parent == "BA.5"
 
 def test_collapse_column():
-    assert ['B', 'B'] == collapsor.collapse_column(("BA.5", "BA.2"), ["B"])
+    assert ["B", "B"] == collapsor.collapse_column(("BA.5", "BA.2"), ["B"])
+
 
 def test_uncompress_column():
-    assert ['B.1.1.529.5', 'B'] == collapsor.uncompress_column(("BA.5", "B"))
+    assert ["B.1.1.529.5", "B"] == collapsor.uncompress_column(("BA.5", "B"))
+
 
 def test_collapse_recombinants():
     assert ["Recombinant", "Recombinant", "XBF.7"] == collapsor.collapse_column(
-        ['XBB.1.5.13', 'EL.1', "XBF.7.1"], 
-        ["Recombinant", "XBF.7"]
+        ["XBB.1.5.13", "EL.1", "XBF.7.1"], ["Recombinant", "XBF.7"]
     )
 
+
 def test_recombinant_sublineage():
-    compressed_lineage = 'EL.1'
+    compressed_lineage = "EL.1"
     assert "XBB.1" == collapsor.collapse(compressed_lineage, ["XBB.1"])
+
+
+def test_expand():
+    assert "B.1.1.529:BA.2.75.3:BM.4.1.1:CH.1.1.17" == collapsor.expand(
+        "CH.1.1.17"
+    )
+    assert "B.1.1.529:BA.2.75.3:BM.4.1.1:CH.1.1" == collapsor.expand(
+        "CH.1.1"
+    )
+    assert "B.1.1.529:BA.5" == collapsor.expand(
+        "B.1.1.529.5"
+    )
+
+
+def test_expand_column():
+    assert [
+        "B.1.1.529:BA.2.75.3:BM.4.1.1:CH.1.1",
+        "B.1.1.529:BA.2.75.3:BM.4.1.1:CH.1.1.17",
+    ] == collapsor.expand_column(("CH.1.1", "CH.1.1.17"))
