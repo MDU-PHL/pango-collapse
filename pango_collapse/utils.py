d334289a0a7629c0469dd241e2e40d60e5411cf8
Wytamma Wirth <wytamma.wirth@me.com>
2024-03-13 10:30:03 +1100

    :art: --url option is deprecated

--- pango_collapse/utils.py
+++ pango_collapse/utils.py
@@ -8,7 +8,7 @@ def load_potential_parents_from_url(
     import urllib.request
     potential_parents = []
     try:
-        with urllib.request.urlopen(url) as data:
+        with urllib.request.urlopen(str(url)) as data:
             potential_parents += data.read().decode("utf-8").strip().split("\n")
     except URLError:
         raise URLError(f"Could not download collapse file from {url}.")
@@ -16,5 +16,5 @@ def load_potential_parents_from_url(
 
 def load_potential_parents_from_file(collapse_file: str) -> List[str]:
     with open(collapse_file) as f:
-        potential_parents = [l.strip() for l in f.readlines() if l.strip()]
+        potential_parents = [line.strip() for line in f.readlines() if line.strip()]
     return [line for line in potential_parents if not line.startswith("#")]
